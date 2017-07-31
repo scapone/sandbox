a70a834d2fa511045ce8d671a5bb7cfb1068bf86
Serhii H <sergey.gaponenko@gmail.com>
2017-07-31 13:46:05 +0300

    Gulp JS Crash Course

---
+++ Gulp/gulpexapp/gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+
+gulp.task('message', function() {
+  return console.log('Gulp is running...');
+});
+
+gulp.task('copyHtml', function() {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('imageMin', () =>
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+
+gulp.task('sass', function() {
+  gulp.src('src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('scripts', function() {
+  gulp.src('src/js/*.js')
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('default', ['message', 'copyHtml', 'imageMin', 'sass', 'scripts']);
+
+gulp.task('watch', function() {
+  gulp.watch('src/js/*.js', ['scripts']);
+  gulp.watch('src/images/*', ['imageMin']);
+  gulp.watch('src/sass/*.scss', ['sass']);
+  gulp.watch('src/*.html', ['copyHtml']);
+});
---
+++ Gulp/gulpexapp/src/js/file1.js
@@ -0,0 +1,6 @@
+// Console Log 1
+console.log('This is file One');
+
+
+// Console Log 1.1
+console.log('This is file 1.1');
