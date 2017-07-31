37c511785c21a2e894649bf6fd5a6ab1eb899101
Serhii <sergey.gaponenko@gmail.com>
2017-07-31 14:34:56 +0300

    Webpack Crash Course

---
+++ Webpack/webpack_app/src/js/app.js
@@ -0,0 +1,7 @@
+require('../css/style.css')
+let people = require('./people.js');
+let $ = require('jquery')
+
+$.each(people, function(key, value) {
+  $('body').append('<h1>' + people[key].name + '</h1>')
+});
---
+++ Webpack/webpack_app/src/js/people.js
@@ -0,0 +1,7 @@
+let people = [
+  { name: 'John Doe' },
+  { name: 'Steve Smith' },
+  { name: 'Carol Williams' }
+];
+
+module.exports = people;
